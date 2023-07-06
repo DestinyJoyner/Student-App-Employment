94957a0bf7265e417cf4be70c6ac0e6cb862a8a9
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-06 12:57:54 -0400

    set up react app, student card component

---
+++ src/Components/StudentCard.js
@@ -0,0 +1,22 @@
+import "./StudentCard.css"
+function StudentCard(props) {
+    return (
+        <div className = "studentCard">
+        <div className="studentCard_image-container">
+          <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png" className="studentCard_image" />
+        </div>
+        
+        <section className = "student-card-info">
+          <h2 className = "student-card-name"> Ingaberg Orton </h2>
+          <div className = "student-card-info-details">
+            <span>Email:</span>
+            <span>Company:</span>
+            <span>Skill:</span>
+            <span>Average: 88.75%</span>
+          </div>
+        </section>
+      </div>
+    );
+}
+
+export default StudentCard;
